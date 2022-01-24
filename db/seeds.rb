e20f47ea693c3dd0dd785dc4b194fb889a433b04
Orlando Yurivilca <sergioyv@gmail.com>
2022-01-24 18:02:45 -0500

    products index

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts "Cleaning database..."
+Product.destroy_all
+
+puts 'creating products'
+Product.create(name: 'Polo Blanco', description: 'polo de algodon pima', category: 'Polos' , price: 10, stock: 1, user_id:1)
+Product.create(name: 'Jean Azul', description: 'jean tipo vaquero', category: 'Jeans' , price: 30, stock: 2, user_id:1)
+Product.create(name: 'Camisa Lacoste Celeste', description: 'camisa modelo lacoste azul', category: 'Camisas' , price: 45, stock: 4, user_id:1)
+Product.create(name: 'Polo Negro', description: 'polo negro de algodon', category: 'Polos' , price: 20, stock: 2, user_id:2)
+puts "Finished!"
