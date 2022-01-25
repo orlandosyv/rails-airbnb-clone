2bb6df9e911a9514b169c0d9270f73f2ea7f5ab3
Daniel Herrera <daniel97hh@gmail.com>
2022-01-25 11:33:13 -0500

    rutas ready

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  resources :products do
+    resources :orders, only: %i[new create]
+  end
+
+  resources :orders, only: %i[show] do
+    resources :reviews, only: %i[new create]
+  end
 end
