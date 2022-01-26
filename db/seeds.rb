502f5bcb617037e77e4b12af8dda86b0547dfcdf
Orlando Yurivilca <sergioyv@gmail.com>
2022-01-26 10:56:40 -0500

    seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -16,9 +16,5 @@ Product.create(name: 'Camisa Tommy gris', description: 'camisa modelo tommy gris
 Product.create(name: 'Polo Negro', description: 'polo negro de algodon', category: 'Polos' , price: 20, stock: 2, user_id:1)
 Product.create(name: 'Jean Negro', description: 'Jean negro', category: 'Jeans' , price: 70, stock: 4, user_id:1)
 Product.create(name: 'Camisa D&G azul', description: 'Camisa D&G azul claro', category: 'Camisas' , price: 55, stock: 2, user_id:1)
-<<<<<<< HEAD
 Product.create(name: 'Vestido rojo seda', description: 'Vestido rojo seda', category: 'Vestidos' , price: 100, stock: 1, user_id:1)
 puts "Finished!"
-=======
-puts "Finished!"
->>>>>>> master
