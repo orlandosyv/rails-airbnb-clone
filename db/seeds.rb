ba91d208823a269f212a038a891a3013fab37aa2
Orlando Yurivilca <sergioyv@gmail.com>
2022-01-26 16:05:02 -0500

    cloudinary

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,12 +9,13 @@ puts "Cleaning database..."
 Product.destroy_all
 
 puts 'creating products'
-Product.create(name: 'Polo Blanco', description: 'polo de algodon pima', category: 'Polos' , price: 10, stock: 1, user_id:1)
+polo = Product.create(name: 'Polo Blanco', description: 'polo de algodon pima', category: 'Polos' , price: 10, stock: 1, user_id:1)
+nico.photo.attach(io: open("https://res.cloudinary.com/wagon/image/upload/c_fill,g_face,h_600,w_600/jjnpprepy6ktoh1afgdz.jpg"), filename: "nicolas.png", content_type: "image/jpeg") nico.save! 
 Product.create(name: 'Jean Azul', description: 'jean tipo vaquero', category: 'Jeans' , price: 30, stock: 2, user_id:1)
 Product.create(name: 'Camisa Lacoste Celeste', description: 'camisa modelo lacoste azul', category: 'Camisas' , price: 45, stock: 4, user_id:1)
 Product.create(name: 'Camisa Tommy gris', description: 'camisa modelo tommy gris oscuro', category: 'Camisas' , price: 55, stock: 1, user_id:1)
 Product.create(name: 'Polo Negro', description: 'polo negro de algodon', category: 'Polos' , price: 20, stock: 2, user_id:1)
 Product.create(name: 'Jean Negro', description: 'Jean negro', category: 'Jeans' , price: 70, stock: 4, user_id:1)
 Product.create(name: 'Camisa D&G azul', description: 'Camisa D&G azul claro', category: 'Camisas' , price: 55, stock: 2, user_id:1)
-Product.create(name: 'Vestido rojo seda', description: 'Vestido rojo seda', category: 'Vestidos' , price: 100, stock: 1, user_id:1)
+Product.create(name: 'Vestido rojo seda', description: 'Vestido rojo seda', category: 'Vestidos' , price: 100, stock: 1,  user_id:1)
 puts "Finished!"
